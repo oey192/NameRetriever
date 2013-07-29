680d469e89c4d5cb9828eb227d421b0848cda8b8
Orion <oeystanger@gmail.com>
2013-07-28 23:53:11 -0600

    API complete barring testing

--- src/com/andoutay/nameretriever/NRAPI.java
+++ src/com/andoutay/nameretriever/NRAPI.java
@@ -6,6 +6,7 @@ public class NRAPI
 {
 	private static NRAPI api = null;
 	private static NREssentialsManager em;
+	
 	protected NRAPI()
 	{
 		em = NameRetriever.getEssManager();
@@ -26,69 +27,68 @@ public class NRAPI
 	
 	/**
 	 * Returns the nickname associated with the given Minecraft username on the current server.
-	 * If the given player has no nickname, it returns exactly what was passed in via the
-	 * username parameter. If no user with the given username is found, it returns null.
+	 * <br>
+	 * If the given player has no nickname, the player's Minecraft username is returned.
+	 * <br>
+	 * If more than one user with the given username is found, an empty string is returned
+	 * <br>
+	 * If no user with the given username is found, null is returned.
+	 * <br>
+	 * This function is disk intensive. It's recommended to make an asynchronous call to it
 	 * 
 	 * @param 	username	The full username of the player you would like to find a nickname for
 	 * @return				the nickname associated with the given Minecraft username
-	 * @see					NRAPI#getNicks(String) getNicks
+	 * @see					NRAPI#getNicks(String) getNicks(String)
 	 */
 	public String getNick(String username)
 	{
-		//return em.getNickname(username);
-		return null;
+		String ans = "";
+		HashMap<String, String> names = em.getOnAndOfflineNicksForName(username);
+		if (names.size() == 1)
+			ans = names.get(username);
+		else if (names.size() < 1)
+			ans = null;
+			
+		return ans;
 	}
 	
 	/**
 	 * Returns a HashMap of Minecraft usernames and nicknames based on partial matches
 	 * for usernames. The HashMap maps username -> nickname and will contain all
 	 * matches found for the partial username from players who have played on the
-	 * current server. If there are no matches, an empty HashMap is returned.
-	 * For players who have no nickname, it will contain an entry like
-	 * whosyourdaddy -> whosyourdaddy
+	 * current server.
+	 * <br>
+	 * If a player has not been assigned a nickname on the server, their "nickname" will be the same as their Minecraft username
+	 * <br>
+	 * If there are no matches, an empty HashMap is returned.
+	 * <br>
+	 * This function is disk intensive. It's recommended to make an asynchronous call to it
 	 * 
 	 * @param partial	A partial or full Minecraft username to search for associated nicknames
-	 * @return			A HashMap mapping Minecraft usernames to nicknames containing username matches for partial
+	 * @return			A HashMap mapping Minecraft usernames to nicknames containing username matches for <b>partial</b>
+	 * @see				NRAPI#getNick(String) getNick(String)
 	 */
 	public HashMap<String, String> getNicks(String partial)
 	{
-		//return em.getNicknames(partial);
-		return null;
+		return em.getOnAndOfflineNicksForName(partial);
 	}
 	
 	/**
 	 * Returns a HashMap of Minecraft usernames and nicknames based on partial matches
 	 * for nicknames. The HashMap maps username -> nickname and will contain all
 	 * matches found for the partial nickname from players that have nicknames on the
-	 * current server. If there are no matches, an empty HashMap will be returned.
+	 * current server.
+	 * <br>
+	 * If there are no matches, an empty HashMap will be returned.
+	 * <br>
+	 * This function is disk intensive. It's recommended to make an asynchronous call to it
 	 * 
 	 * @param partial	A partial or full nickname. Should not include a tilde or any other "Hi I'm a nickname" indicators
-	 * @return			A HashMap mapping Minecraft usernames to nicknames containing nickname matches for partial
-	 * @see				NRAPI#getAllNames(String) getAllNames
+	 * @return			A HashMap mapping Minecraft usernames to nicknames containing nickname matches for <b>partial</b>
 	 */
 	public HashMap<String, String> getNames(String partial)
 	{
-		//return em.getNames(partial);
-		return null;
-	}
-	
-	/**
-	 * Returns a HashMap of Minecraft usernames and nicknames based on partial matches for nicknames.
-	 * The HashMap maps username -> nickname and will contain all matches found for the partial
-	 * nickname from players how have played on the current server.
-	 * <p>
-	 * The HashMap will contain any matches for display names whether the player has a nickname or not
-	 * <p>
-	 * If there are no matches, an empty HashMap will be returned.
-	 * 
-	 * @param partial	A partial or full nickname. Should not include a tilde or any other "Hi I'm a nickname" indicators
-	 * @return			A HashMap mapping Minecraft usernames to nicknames containing nickname matches for partial
-	 * @see				NRAPI#getNames(String) getNames
-	 */
-	public HashMap<String, String> getAllNames(String partial)
-	{
-		//return em.getAllNames(partial);
-		return null;
+		return em.getOnAndOfflineNamesForNick(partial);
 	}
 
 }
--- src/com/andoutay/nameretriever/NREssentialsManager.java
+++ src/com/andoutay/nameretriever/NREssentialsManager.java
@@ -28,6 +28,117 @@ public class NREssentialsManager
 		if (NameRetriever.essPresent) e = (Essentials)plugin.getServer().getPluginManager().getPlugin("Essentials");
 	}
 	
+	private HashMap<String, String> getOnlineNamesForNick(String partial)
+	{
+		HashMap<String, String> ans = new HashMap<String, String>();
+		
+		for (Player p: plugin.getServer().getOnlinePlayers())
+			if (p.getDisplayName().toLowerCase().contains(partial.toLowerCase()) && !p.getName().equals(p.getDisplayName()))
+				ans.put(p.getName(), p.getDisplayName());
+		
+		return ans;
+	}
+	
+	private HashMap<String, String> getOfflineNamesForNick(String partial)
+	{
+		HashMap<String, String> ans = new HashMap<String, String>();
+		
+		//We need Essentials to get offline nicknames so if it's not loaded, just return an empty HashMap
+		if (!NameRetriever.essPresent) return ans;
+		
+		File[] files = e.getDataFolder().listFiles(new FilenameFilter() { public boolean accept(File dir, String filename) { return filename.endsWith("userdata");}});
+		if (files.length == 1)
+		{
+			File f = files[0];
+			for (File yml : f.listFiles())
+				if (yml.getName().endsWith(".yml"))
+				{
+					String temp = YamlConfiguration.loadConfiguration(yml).getString("nickname");
+					if (temp != null && temp.toLowerCase().contains(partial.toLowerCase()))
+						ans.put(plugin.getServer().getOfflinePlayer(yml.getName().substring(0, yml.getName().length() - 4)).getName(), "~" + temp);
+				}
+		}
+		else
+			log.warning(NameRetriever.logPref + "Could not find Essentials' userdata folder when attempting to retrieve nicknames");
+		
+		return ans;
+	}
+	
+	private HashMap<String, String> getOnlineNicksForName(String partial)
+	{
+		HashMap<String, String> ans = new HashMap<String, String>();
+
+		//try to get nick based on partial match from online players - just in case essentials hasn't saved a file for an online player
+		for (Player player : plugin.getServer().getOnlinePlayers())
+			if (player.getName().toLowerCase().contains(partial.toLowerCase()))
+				ans.put(player.getName(), player.getDisplayName());
+		
+		return ans;
+	}
+	
+	private HashMap<String, String> getOfflineNicksForName(String partial)
+	{
+		HashMap<String, String> ans = new HashMap<String, String>();
+		
+		//We need Essentials to get offline nicknames so if it's not loaded, just return an empty HashMap
+		if (!NameRetriever.essPresent) return ans;
+		
+		File[] files = e.getDataFolder().listFiles(new FilenameFilter() { public boolean accept(File dir, String filename) { return filename.endsWith("userdata");}});
+		if (files.length == 1)
+		{
+			File f = files[0];
+			for (File yml : f.listFiles())
+				if (yml.getName().endsWith(".yml"))
+				{
+					String fname = yml.getName().substring(0, yml.getName().length() - 4);
+					if (fname.toLowerCase().contains(partial.toLowerCase()))
+					{
+						String temp1, temp2;
+
+						temp1 = plugin.getServer().getOfflinePlayer(fname).getName();
+						temp2 = YamlConfiguration.loadConfiguration(yml).getString("nickname");
+						if (temp2 == null)
+							temp2 = temp1;
+						else
+
+						ans.put(temp1, temp2);
+					}
+				}
+		}
+		else
+			log.warning(NameRetriever.logPref + "Could not find Essentials' userdata folder when attempting to retrieve nicknames");
+		
+		return ans;
+	}
+	
+	public HashMap<String, String> getOnAndOfflineNamesForNick(String partial)
+	{
+		HashMap<String, String> ans = getOnlineNamesForNick(partial);
+		ans.putAll(getOfflineNamesForNick(partial));
+		return ans;
+	}
+	
+	public HashMap<String, String> getOnAndOfflineNicksForName(String partial)
+	{
+		HashMap<String, String> ans = getOnlineNicksForName(partial);
+		ans.putAll(getOfflineNicksForName(partial));
+		return ans;
+	}
+	
+	public HashMap<String, String> getNamesForNick(String partial)
+	{
+		HashMap<String, String> ans = getOnlineNamesForNick(partial);
+		if (NRConfig.getOfflineAlways || ans.isEmpty()) ans.putAll(getOfflineNamesForNick(partial));
+		return ans;
+	}
+	
+	public HashMap<String, String> getNicksForName(String partial)
+	{
+		HashMap<String, String> ans = getOnlineNicksForName(partial);
+		if (NRConfig.getOfflineAlways || ans.isEmpty()) ans.putAll(getOfflineNicksForName(partial));
+		return ans;
+	}
+	
 	public void getName(final String name, final CommandSender s)
 	{
 		HashMap<String, String> ans = new HashMap<String, String>();
